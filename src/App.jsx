727a0d6fec1e0aa60bbdf723d967ca4b0bae7634
muratdemirkiran33 <muratdmrkrn1@gmail.com>
2025-03-10 02:49:58 +0300

    Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     test
-    </>
-  )
+    return (
+        <div className="min-h-screen bg-black flex items-center justify-center p-5">
+            <div className="max-w-md w-full bg-black border-2 border-white rounded-lg p-8 text-center">
+                <h1 className="text-3xl font-sem text-white mb-4">Hoşgeldiniz</h1>
+                <p className="text-xl font-bold font text-white">Ben Murat Demirkıran</p>
+            </div>
+        </div>
+    )
 }
 
 export default App
