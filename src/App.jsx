958fe43401e92eeacb91a2c697a43a43f5262ca2
muratdemirkiran33 <muratdmrkrn1@gmail.com>
2025-03-10 03:32:49 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css'
 
 function App() {
-    return (
-        <div className="min-h-screen bg-black flex items-center justify-center p-5">
-            <div className="max-w-md w-full bg-black border-2 border-white rounded-lg p-8 text-center">
-                <h1 className="text-3xl font-sem text-white mb-4">Hoşgeldiniz</h1>
-                <p className="text-xl font-bold font text-white">Ben Murat Demirkıran</p>
-            </div>
+    return (<div className="min-h-screen bg-black w-full flex items-center justify-center p-5">
+        <div className="max-w-md w-full bg-black border-2 border-white rounded-lg p-8 text-center">
+            <h1 className="text-3xl font-sem text-white mb-4">Hoşgeldiniz</h1>
+            <p className="text-xl font-semibold font text-white">Ben Murat Demirkıran</p>
         </div>
-    )
+    </div>)
 }
 
 export default App
