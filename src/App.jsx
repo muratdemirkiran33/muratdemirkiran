94ffc3e24c77d5e4c3e110d3bf0c46c9f0f14d0f
muratdemirkiran33 <muratdmrkrn1@gmail.com>
2025-11-29 20:26:41 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import { CustomCursor } from './components/CustomCursor'
+import { ClickSpark } from './components/ClickSpark'
 
 function App() {
     return (
         <div className="min-h-dvh bg-black w-full">
-
+            <div className="text-primary">Murat Demirkiran</div>
+            <CustomCursor />
+            <ClickSpark />
         </div>
     )
 }
