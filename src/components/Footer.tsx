d7e13b330494abe9184ca7e12e71b37dcbd641d7
muratdemirkiran33 <muratdmrkrn1@gmail.com>
2025-11-30 05:57:01 +0300

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,12 +35,12 @@ const Footer = () => {
                 <p className="text-lg">Have a project in mind?</p>
                 <a
                     href={`mailto:${GENERAL_INFO.email}`}
-                    className="text-3xl sm:text-4xl font-anton inline-block mt-5 mb-10 hover:underline"
+                    className="text-2xl sm:text-4xl font-anton inline-block mt-5 mb-10 hover:underline"
                 >
                     {GENERAL_INFO.email}
                 </a>
 
-               
+
             </div>
         </footer>
     );
